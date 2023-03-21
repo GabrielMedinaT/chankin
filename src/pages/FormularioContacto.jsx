6c30c840b523167a9d7b1b3fbab3ec25a95701ce
GabrielMedinaT <116290646+GabrielMedinaT@users.noreply.github.com>
2023-03-21 22:34:25 +0000

    Cambio en el formulario para adaptar el emailjs

--- src/pages/FormularioContacto.jsx
+++ src/pages/FormularioContacto.jsx
@@ -11,12 +11,19 @@ const FormularioContacto = () => {
         "service_zdno5qt",
         "template_q75lp48",
         e.target,
-        "user_y-et0LXZbs95kUCDP"
+        "3aM_N5iXX48JCZkh9"
       )
-      .then((res) => {
-        console.log(res);
-      });
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
   }
+
   return (
     <div className="componente">
       <h1>¿Tiene alguna duda? Estámos a sus órdenes:</h1>
@@ -25,12 +32,18 @@ const FormularioContacto = () => {
         <label htmlFor="motivo" className="form-label">
           Motivo de su mensaje
         </label>
-        <select class="form-select" aria-label="Default select example">
-          <option value="opcion1">Información sobre actividades</option>
-          <option value="opcion2">Consulta con profesor</option>
-          <option value="opcion3">Hacer sugerencia</option>
+        <select
+          class="form-select"
+          aria-label="Default select example"
+          name="opcion"
+        >
+          <option value="Informacion sobre actividades">
+            Información sobre actividades
+          </option>
+          <option value="Consulta con profesor">Consulta con profesor</option>
+          <option value="Hacer una sugerencia">Hacer sugerencia</option>
         </select>
-        <label htmlFor="nombre" className="form-label">
+        <label htmlFor="nombre" className="form-label" name="nombre">
           Nombre:
         </label>
         <input
@@ -46,9 +59,10 @@ const FormularioContacto = () => {
           type="email"
           className="form-control form-control-sm"
           id="email"
+          name="email"
         />
         <br></br>
-        <label htmlFor="mensaje" className="form-label">
+        <label htmlFor="mensaje" className="form-label" name="mensaje">
           Introduzca su mensaje aquí:
         </label>
         <textarea
