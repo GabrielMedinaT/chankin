5f61141da6ec308b5843dd0f904ba29050c86531
Aleksandar Halachev <alekbalek1@gmail.com>
2023-03-23 00:31:46 +0000

    Pruebas de animaciones

--- src/pages/Acerca.jsx
+++ src/pages/Acerca.jsx
@@ -11,16 +11,30 @@ function ScrollAnimation() {
   return (
     <div>
       <h1>Acerca de nosotros</h1>
-      <div className="animation" data-aos="fade-up"></div>
-      <div className="animation" data-aos="fade-down"></div>
+      <div
+        className="animation"
+        data-aos="fade-up"
+        data-aos-duration="3000"
+      ></div>
+      <div className="animation" data-aos="fade-down">
+        {" "}
+      </div>
       <div className="animation" data-aos="fade-rigth"></div>
       <div className="animation" data-aos="fade-left"></div>
       <div className="animation" data-aos="flip-up"></div>
       <div className="animation" data-aos="flip-down"></div>
-      <div className="animation" data-aos="flip-rigth"></div>
+      <div
+        className="animation"
+        data-aos="flip-right"
+        data-aos-easing="ease-out-cubic"
+        data-aos-duration="1000"
+      ></div>
       <div className="animation" data-aos="fade-left"></div>
       <div className="animation" data-aos="zoom-in"></div>
       <div className="animation" data-aos="zoom-out"></div>
+      <div className="animation" data-aos="slide-up"></div>
+      <div className="animation" data-aos="example-anim2"></div>
+      <div className="animation" data-aos="zoom-out-right"></div>
     </div>
   );
 }
